e99ec231daa87a37cb011e46449db1bd0140b047
Boran Seckin <me@boranseckin.com>
2021-01-01 20:54:54 +0300

    Initial Commit

---
+++ Lake/LakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LakeApp.swift
+//  Lake
+//
+//  Created by Boran Seckin on 2021-01-01.
+//
+
+import SwiftUI
+
+@main
+struct LakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
